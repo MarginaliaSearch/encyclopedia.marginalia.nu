8d210330ef23bf992a119ca515714d3d9dff5c02
Viktor Lofgren <vlofgren@marginalia.nu>
2023-08-31 11:03:17 +0200

    Fix encoding problem with searches that include spaces.

--- src/main/java/nu/marginalia/encyclopedia/svc/FindArticleService.java
+++ src/main/java/nu/marginalia/encyclopedia/svc/FindArticleService.java
@@ -27,7 +27,7 @@ public class FindArticleService {
     }
 
     public String handle(Request request, Response response) throws IOException {
-        var url = request.params(":url");
+        var url = request.params(":url").replace('+', ' ');
         var searchKey = request.queryParamOrDefault("start", url);
 
         response.header("Content-Encoding", "gzip");
